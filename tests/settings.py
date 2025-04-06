09d0bba051001c7cd84277ee1771dbd8afc7a25c
Иларион Ерченко <turolretar@gmail.com>
2025-04-06 21:32:25 +0300

    :white-check-mark: fix settings tests

--- tests/settings.py
+++ tests/settings.py
@@ -13,17 +13,17 @@ class TestSettingsWithPythonValues(unittest.TestCase):
     """
 
     def test_well_formed_values(self):
-        settings = Settings(cdn_host=HttpUrl("http://cdn-domain"), redirect_ratio=parse_redirect_ratio("1:10"))
+        settings = Settings(cdn_host=HttpUrl("http://cdn-domain"), redirect_ratio=parse_redirect_ratio("10:1"))
         self.assertEqual(settings.cdn_host.host, "cdn-domain")
-        self.assertEqual(settings.redirect_ratio, Fraction(1, 10))
+        self.assertEqual(settings.redirect_ratio, Fraction(10, 1))
 
     def test_ill_formed_value_for_cdn_host(self):
         with self.assertRaises(ValidationError):
-            Settings(cdn_host=HttpUrl("123456789"), redirect_ratio=parse_redirect_ratio("1:10"))
+            Settings(cdn_host=HttpUrl("123456789"), redirect_ratio=parse_redirect_ratio("10:1"))
 
     def test_non_http_cdn_host(self):
         with self.assertRaises(ValidationError):
-            Settings(cdn_host=HttpUrl("ftp://cdn-domain"), redirect_ratio=parse_redirect_ratio("1:10"))
+            Settings(cdn_host=HttpUrl("ftp://cdn-domain"), redirect_ratio=parse_redirect_ratio("10:1"))
 
     def test_ill_formed_value_for_redirect_ratio(self):
         with self.assertRaises(ValidationError):
@@ -31,11 +31,11 @@ class TestSettingsWithPythonValues(unittest.TestCase):
 
     def test_negative_cdn_redirect_count(self):
         with self.assertRaises(ValidationError):
-            Settings(cdn_host=HttpUrl("http://cdn-domain"), redirect_ratio=parse_redirect_ratio("-1:10"))
+            Settings(cdn_host=HttpUrl("http://cdn-domain"), redirect_ratio=parse_redirect_ratio("-10:1"))
 
     def test_negative_origin_servers_redirect_count(self):
         with self.assertRaises(ValidationError):
-            Settings(cdn_host=HttpUrl("http://cdn-domain"), redirect_ratio=parse_redirect_ratio("1:-10"))
+            Settings(cdn_host=HttpUrl("http://cdn-domain"), redirect_ratio=parse_redirect_ratio("10:-1"))
 
 
 class TestSettingsFromEnv(unittest.TestCase):
@@ -43,18 +43,18 @@ class TestSettingsFromEnv(unittest.TestCase):
     Настройки устанавливаются из значений переменных окружения.
     """
 
-    @patch_environ(BALANCER_CDN_HOST="http://cdn-domain", BALANCER_REDIRECT_RATIO="1:10")
+    @patch_environ(BALANCER_CDN_HOST="http://cdn-domain", BALANCER_REDIRECT_RATIO="10:1")
     def test_well_formed_values(self):
         settings = Settings()  # type: ignore
         self.assertEqual(settings.cdn_host.host, "cdn-domain")
-        self.assertEqual(settings.redirect_ratio, Fraction(1, 10))
+        self.assertEqual(settings.redirect_ratio, Fraction(10, 1))
 
-    @patch_environ(BALANCER_CDN_HOST="123456789", BALANCER_REDIRECT_RATIO="1:10")
+    @patch_environ(BALANCER_CDN_HOST="123456789", BALANCER_REDIRECT_RATIO="10:1")
     def test_ill_formed_value_for_cdn_host(self):
         with self.assertRaises(ValueError):
             Settings()  # type: ignore
 
-    @patch_environ(BALANCER_CDN_HOST="ftp://cdn-domain", BALANCER_REDIRECT_RATIO="1:10")
+    @patch_environ(BALANCER_CDN_HOST="ftp://cdn-domain", BALANCER_REDIRECT_RATIO="10:1")
     def test_non_http_cdn_host(self):
         with self.assertRaises(ValueError):
             Settings()  # type: ignore
@@ -64,12 +64,12 @@ class TestSettingsFromEnv(unittest.TestCase):
         with self.assertRaises(ValidationError):
             Settings()  # type: ignore
 
-    @patch_environ(BALANCER_CDN_HOST="http://cdn-domain", BALANCER_REDIRECT_RATIO="-1:10")
+    @patch_environ(BALANCER_CDN_HOST="http://cdn-domain", BALANCER_REDIRECT_RATIO="-10:1")
     def test_negative_cdn_redirect_count(self):
         with self.assertRaises(ValidationError):
             Settings()  # type: ignore
 
-    @patch_environ(BALANCER_CDN_HOST="http://cdn-domain", BALANCER_REDIRECT_RATIO="1:-10")
+    @patch_environ(BALANCER_CDN_HOST="http://cdn-domain", BALANCER_REDIRECT_RATIO="10:-1")
     def test_negative_origin_servers_redirect_count(self):
         with self.assertRaises(ValidationError):
             Settings()  # type: ignore
