788994fd2487228043ca63c6fac9c7fbd99e3762
Иларион Ерченко <turolretar@gmail.com>
2025-04-08 11:23:10 +0300

    :recycle: extract RPS test into separate module
    Completely reworked the test:
    
    - Remove unnecessary `unittest` test case class. Now all functions are
    module-level.
    - Replaced `httpx` library with `aiohttp` for faster requests.
    - Added chunking of async tasks and chunk execution in threads.
    
    Misc:
    - Moved utility functions in to `tests.utils` module.
    - Added `rps-test` PDM script to run the test separately from unit/API
    tests.

---
+++ tests/rps_test.py
@@ -0,0 +1,117 @@
+"""
+Тестирование показателя количества запросов в секунду. Для демонстрации того, что сервис может обработать не менее 1000 запросов в секунду вполне подходит.
+"""
+
+import asyncio
+import itertools
+import math
+import os
+import statistics
+import subprocess
+import time
+from concurrent import futures
+from pprint import pprint
+
+import aiohttp
+
+from tests.utils import get_random_video_url, wait_for_balancer_api
+
+balancer_host = "127.0.0.1:3000"
+balancer_env = {**os.environ.copy(), "BALANCER_CDN_HOST": "http://cdn-host", "BALANCER_REDIRECT_RATIO": "3:1"}
+balancer_start_cmd = [
+    "gunicorn",
+    "wink_test.main:app",
+    "--bind",
+    balancer_host,
+    "--workers",
+    # 4 ядра * 2 потока + 1 воркер для задач FastAPI
+    "9",
+    "--worker-class",
+    "uvicorn.workers.UvicornWorker",
+]
+
+
+async def make_request(client: aiohttp.ClientSession, index: int):
+    random_video_url = get_random_video_url(index + 1)
+    response = await client.get("/", params={"video": random_video_url}, allow_redirects=False)
+    return response.headers.get("location")
+
+
+async def make_requests(index_range: range):
+    async with aiohttp.ClientSession(base_url=f"http://{balancer_host}", raise_for_status=True) as client:
+        await wait_for_balancer_api(client)
+        tasks = [make_request(client, i) for i in index_range]
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        exc: list[Exception] = []
+        for result in results:
+            match result:
+                case Exception():
+                    exc.append(result)
+                case _:
+                    pass
+
+        pprint(exc)
+
+        if len(exc) / len(results) > 0.1:
+            raise ValueError
+
+
+def requests_maker_thread_main(index_range: range):
+    asyncio.run(make_requests(index_range))
+
+
+def main():
+    rps_values: list[int] = []
+
+    with subprocess.Popen(
+        balancer_start_cmd,
+        env=balancer_env,
+        shell=False,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    ) as balancer_process:
+        number_of_requests = 1000
+        requests_per_thread = 300
+        workers_count = math.ceil(number_of_requests / requests_per_thread)
+
+        with futures.ThreadPoolExecutor(max_workers=workers_count) as executor:
+            for round_index in range(1, 11):
+                index_ranges = [
+                    range(start_index, stop_index)
+                    for start_index, stop_index in itertools.pairwise(
+                        range(0, number_of_requests + 1, requests_per_thread)
+                    )
+                ]
+                start_time = time.time()
+
+                try:
+                    result_futures = [executor.submit(requests_maker_thread_main, x) for x in index_ranges]
+                    for fut in futures.as_completed(result_futures):
+                        fut.result()
+                    exec_time = time.time() - start_time
+
+                    requests_per_second = int(number_of_requests / exec_time)
+                    rps_values.append(requests_per_second)
+
+                    print(
+                        f"[Round {round_index}] Balancer handled {number_of_requests} requests in {exec_time:.3f} seconds. RPS: {requests_per_second}"
+                    )
+                except Exception as e:
+                    print(f"[Round {round_index}] Round failed - {type(e).__name__}: {e}")
+                finally:
+                    time.sleep(0.5)
+
+        if len(rps_values) > 0:
+            median_rps = statistics.median(rps_values)
+            print(f"Median PRS: {median_rps}")
+        else:
+            print("All rounds failed.")
+
+        print("Killing balancer process...")
+        balancer_process.terminate()
+        balancer_process.wait()
+
+
+if __name__ == "__main__":
+    main()
--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,12 @@
+import asyncio
 import os
 from unittest import mock
 
-__all__ = ("patch_environ",)
+import aiohttp
+import aiohttp.client_exceptions
+import aiohttp.http_exceptions
+
+__all__ = ("patch_environ", "get_random_video_url", "wait_for_balancer_api")
 
 
 def patch_environ(**kwargs: str):
@@ -9,3 +14,30 @@ def patch_environ(**kwargs: str):
     Декоратор для замены значений переменных окружения при выполнении теста.
     """
     return mock.patch.dict(os.environ, kwargs)
+
+
+def get_random_video_url(video_id: int):
+    """
+    Генерирует URL видео-файла на origin сервере.
+
+    :param video_id: ID видео.
+    """
+    return f"http://s1.origin-cluster/video/{video_id}/file.m3u8"
+
+
+async def wait_for_balancer_api(client: aiohttp.ClientSession):
+    """
+    Ожидает доступности сервиса балансировщика.
+
+    :param client: HTTP клиент сервиса.
+    """
+
+    for _ in range(5):
+        try:
+            response = await client.get("/health")
+            if response.status == 200:
+                return
+        except aiohttp.client_exceptions.ClientError:
+            await asyncio.sleep(0.5)
+            continue
+    raise TimeoutError
